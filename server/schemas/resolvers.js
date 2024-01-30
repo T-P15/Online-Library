fbb50e2ec99cd3eec1744d28c03a39a18b988b84
mingosis <tom.piccirillo15@gmail.com>
2024-01-30 22:24:48 +1000

    first draft of resolvers.js liable to change

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,75 @@
+const { Book, User } = require('../models');
+const { signToken, AuthenticationError } = require('../utils/auth');
+
+
+const resolvers = {
+    Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+            return User.findOne({ _id: context.user._id }).populate('thoughts');
+            }
+            throw AuthenticationError;
+        },
+
+},
+    Mutation: {
+        addUser: async (parent, { username, email, password }) => {
+            const user = await User.create({ username, email, password });
+            const token = signToken(user);
+            return { token, user };
+          },
+
+          login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+      
+            if (!user) {
+              throw AuthenticationError;
+            }
+      
+            const correctPw = await user.isCorrectPassword(password);
+      
+            if (!correctPw) {
+              throw AuthenticationError;
+            }
+      
+            const token = signToken(user);
+      
+            return { token, user };
+          },
+
+          saveBook: async (parent, { userId, bookInput }, context) => {
+            if (context.user) {
+              return User.findOneAndUpdate(
+                { _id: userId },
+                {
+                  $addToSet: {
+                    savedBooks: { input: bookInput },
+                  },
+                },
+                {
+                  new: true,
+                  runValidators: true,
+                }
+              );
+            }
+            throw AuthenticationError;
+          },
+
+          deleteBook: async (parent, { userId, bookId }, context) => {
+            if (context.user) {
+              return User.findOneAndUpdate(
+                { _id: userId },
+                {
+                  $pull: {
+                    savedBooks: {
+                      bookId: bookId
+                    },
+                  },
+                },
+                { new: true }
+              );
+            }
+            throw AuthenticationError;
+          },
+    }
+}
